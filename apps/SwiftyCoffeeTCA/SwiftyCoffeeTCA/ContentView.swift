337f45d53cdf0a1b21f104a619ae056a3f552af9
Marvin Lee Kobert <marvin.kobert@me.com>
2023-09-06 16:59:19 +0200

    initial commit

---
+++ apps/SwiftyCoffeeTCA/SwiftyCoffeeTCA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftyCoffeeTCA
+//
+//  Created by Marvin Lee Kobert on 06.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apps/SwiftyCoffeeTCA/SwiftyCoffeeTCA/SwiftyCoffeeTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCoffeeTCAApp.swift
+//  SwiftyCoffeeTCA
+//
+//  Created by Marvin Lee Kobert on 06.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCoffeeTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
